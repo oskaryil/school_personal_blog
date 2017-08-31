50c5d10b0a32d0bcb0f03d22fc5859a01523a3f4
Oskar Yildiz <yildizoskar@gmail.com>
2017-08-31 21:13:32 +0200

    added procfile

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Router from "./routes";
-import ContentContainer from "./containers/ContentContainer";
 import logo from "./logo.svg";
 import "./App.css";
 
